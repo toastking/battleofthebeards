9551b6632d89e201d6345457e4a876e259c24775
wmpainter <williammarkpainter@gmail.com>
2013-11-16 22:23:19 -0500

    Update dataAccess.php

--- dataAccess.php
+++ dataAccess.php
@@ -2,3 +2,5 @@ require_once 'vendor\autoload.php';
 use WindowsAzure\Common\ServicesBuilder;
 
 DefaultEndpointsProtocol=[http];AccountName=[beardstorage];AccountKey=[V4IaLOZv6yvlpsO2vIj2QUSXBr7mNJm+iXj3dpy0u2fl6tiWYSzZNIc7STf/izHTmDTG70BbmCn1Tv60sQ4iuw==]
+
+$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
